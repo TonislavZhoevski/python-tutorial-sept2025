92fc9bdb0c9573822cabd55d774dae107ca2b256
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-08 23:32:10 +0300

    lessons 1, 2 and 3

---
+++ lesson_3/meaning.py
@@ -0,0 +1,10 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Ride on!')
+# else:
+#     print('Not today')
+
+# Ternary Operator
+print('Ride on!') if meaning > 10 else print('Not today')
