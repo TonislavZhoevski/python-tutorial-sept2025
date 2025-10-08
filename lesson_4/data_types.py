d38db2ec5b063e0c61d93d1564e04f4870804810
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-08 23:52:51 +0300

    lesson_4 till concatenation

---
+++ lesson_4/data_types.py
@@ -0,0 +1,24 @@
+# String data type
+
+# Literal assignment
+first = "Tony"
+last = "Zhoevski"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+#  constructor function
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# Concatenation
+# fullname = first + " " + last
+# print(fullname)
+first = "Tonislav"
+fullname = first + " " + last
+# print(fullname)
+fullname += "!"
+print(fullname)
