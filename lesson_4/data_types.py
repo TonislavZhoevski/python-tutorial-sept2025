78a2f10ab867570d34892d0cb1ce3e46ace31cc8
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-10 00:41:39 +0300

    engineering nums

--- lesson_4/data_types.py
+++ lesson_4/data_types.py
@@ -67,6 +67,45 @@ print(len(multiline.rstrip()))
 
 print("")
 
-#  Buuld a menu
+#  Build a menu
 title = "menu".upper()
 print(title.center(20, "="))
+print("Coffee".ljust(16, ".") + "$1".rjust(4))
+print("Muffin".ljust(16, ".") + "$2".rjust(4))
+print("Cheescake".ljust(16, ".") + "$4".rjust(4))
+
+print("")
+
+# String index values
+print(first[1])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+# Some methods return boolean data
+print(first.startswith("T"))
+print(first.endswith("Z"))
+
+# Boolean data type
+myvalue = True
+x = bool(False)
+print(type(x))
+print(isinstance(myvalue, bool))
+
+# Numeric data types
+
+# integer type
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price, int))
+#  float type
+gpa = 3.28
+y = float(1.14)
+print(type(gpa))
+
+# complex type
+comp_value = 5+3j
+print(type(comp_value))
+print(comp_value.real)
+print(comp_value.imag)
