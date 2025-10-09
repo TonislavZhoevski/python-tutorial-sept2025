932b8abde971cb2d20d9310a00d1e5034a24e01d
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-09 23:37:56 +0300

    till #  Build a menu row 70

--- lesson_4/data_types.py
+++ lesson_4/data_types.py
@@ -22,3 +22,51 @@ fullname = first + " " + last
 # print(fullname)
 fullname += "!"
 print(fullname)
+
+# Casting anumber to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "I like rock music form the " + decade + "s."
+print(statement)
+
+# Multiple lines
+multiline = '''
+Hey, how are you?                      
+
+I was just chaking in.
+                       All good!
+
+'''
+print(multiline)
+
+# Escaping special characters
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+print(sentence)
+
+# String Methods
+
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
+
+print(multiline.title())
+print(multiline.replace("good", "ok"))
+print(multiline)
+
+print(len(multiline))
+multiline += "                               "
+multiline = "                 " + multiline
+print(len(multiline))
+
+print(len(multiline.strip()))
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
+
+print("")
+
+#  Buuld a menu
+title = "menu".upper()
+print(title.center(20, "="))
